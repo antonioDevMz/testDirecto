44c2d3e913b286f8f475cfeaef184f9b4cf4d464
tonni martinez <antoniomtz251016@outlook.com>
2021-02-22 19:35:13 -0600

    se agregaron todas las funciones

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,25 @@
+const joi   = require('joi');
+const utils = require('../utils');
 
-const test = async (req, res) => {
-    res.status(200).send({
-        success: true,
-        message: 'todo chido'    
-    });
+const schema = joi.object({
+    board : joi.array().required(),
+    word : joi.string().required(),
+});
+
+const test = async ({ body }, res) => {
+    try {
+        await schema.validateAsync(body);
+
+        let oBuilded = await utils.buildJson(body.board);
+        let searchWord = await utils.searchWord(oBuilded, body.word);
+        let status = searchWord ? 200: 404;
+        
+        res.status(status).send({
+            found: searchWord 
+        });
+    } catch (error) {
+        res.status(500).send({success: false, message: error.message})
+    }
 };
 
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,100 @@
-module.exports.logg = "EXPORTS";
+const buildJson = async (data) => {
+    let oCreated = data.map(function(item) {
+        let obj = item.map(function(letter) {
+            return  { letter: letter, used: 0 };
+        });
+        return obj;
+    });
 
-exports.dummy = () => {    
-    return "fin";
+    return oCreated;
 };
 
-export function dummyII() {
-    return "findos";
+const searchFirst = async(obj, oWord) => {
+    let firstLetter = oWord[0];
+    let found       = false;
+
+    for (let ind = 0; ind < obj.length; ind++) {
+        let it = obj[ind];
+        for (let i = 0; i < it.length; i++) {
+            if (it[i].letter == firstLetter) {
+                it[i].used  = true;
+                it[i].array = ind;
+                it[i].index = i;
+                found       = true;
+            }
+        }
+    }
+
+    return {found, obj};
+};
+
+const addCoordinates = async(array, obj, sumArray, sumObjs) => {
+    // console.log(array, sumArray, sumObjs, obj);
+    for (let index = 0; index < array.length; index++) {
+        const obj = array[index];
+        obj.above   = obj.array != 0 ? obj.array -1 : null;
+        obj.down    = obj.array == sumArray-1 ? null : obj.array + 1;
+        obj.left    = obj.index != 0 ? obj.index -1 : null;
+        obj.right   = obj.index == sumObjs-1 ? null : obj.index+1 ;
+        
+    }
+
+    var aa = array[1];
+    console.log(aa);
+    console.log(obj[aa.above][aa.index]);
+    let above = 1;
+    return array;
 }
+
+const searchMore = async(obj, oWord) => {
+
+    console.log(obj, oWord);
+    for (let i=1; i<oWord.length; i++) {
+        const element = oWord[i];
+        console.log(element);
+    }
+};
+
+const searchWord = async(obj, word) => {
+    let sumArray    = obj.length;
+    let sumObjs     = obj[0].length;
+    let oWord       = [...word];;
+    let oFound      = await searchFirst(obj, oWord);
+    var oFirst      =  [];
+
+    if (!oFound.found) {
+        return false;
+    }
+
+    oFound.obj.forEach(async item => {
+        item.forEach(async obj => {
+            if (obj.used) {
+                oFirst.push(obj);
+            }
+        })
+    });
+    
+    let addCoor = await addCoordinates(oFirst, obj, sumArray, sumObjs);
+    // addCoor.forEach(async obj => {
+    //     await searchMore(obj, oWord);
+    // });
+
+
+    // console.log(obj);
+
+    var oo = obj[0]
+    // console.log(oo);
+    let resultado = oo.findIndex( item => item.letter === 'A' );
+
+    // console.log(resultado);
+    // obj.forEach(item => {
+    //     let le = item.filter((i) => i.used == 0);
+    //     console.log(le);
+    // });
+
+};
+
+module.exports = {
+    buildJson,
+    searchWord
+};
